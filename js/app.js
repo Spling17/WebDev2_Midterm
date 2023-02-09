eef156f38d406006b0cc9b8eb477dc9428a001be
haruka <haruka.1017.ishikawa@gmail.com>
2023-02-09 13:33:21 -0800

    backup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 async function getProductsData(){
-  const res = await sendHTTPRequest("GET", "https://gist.githubusercontent.com/Spling17/07221f7ee7e0e878b496a44fcbb9961a/raw/17eee1f1bc68d61e981e48efb9c9fc46d357cad8/products.json")
+  const res = await sendHTTPRequest("GET","https://gist.githubusercontent.com/Spling17/07221f7ee7e0e878b496a44fcbb9961a/raw/3a268bca84b5fb9c63ac552465210bd7ec4640f2/products.json")
   const productTemplate = document.querySelector("template")
   const wrapperProsuct = document.querySelector(".wrapper")
   for (const product of res) {
@@ -15,7 +15,7 @@ async function getProductsData(){
     productElClone.querySelector('.productName').textContent =
       product.name
     productElClone.querySelector('.productPrice').textContent = product.price
-    productElClone.querySelector('.description').texContent = product.description
+    productElClone.querySelector('.description').textContent = product.description
     productElClone.querySelector('.item').id = product.id
     wrapperProsuct.appendChild(productElClone)
   }
@@ -28,6 +28,22 @@ async function sendHTTPRequest(method, url){
 }
 
 window.addEventListener("DOMContentLoaded",getProductsData)
+
+const btn = document.querySelectorAll(".addbtn")
+function getBtnData () {
+  btn.forEach(() =>{
+    console.log(btn)
+  })
+}
+
+/*
+// 
+//ADD TO CART
+function addTOCart(id){
+  console.log(id);
+}
+
+*/
 
 // //SERECT ELEMENTS
 // const productsEl = document.querySelector(".item");
@@ -43,3 +59,16 @@ window.addEventListener("DOMContentLoaded",getProductsData)
 
 
 // alert("abc");
+
+/*
+queryselectorAll を使ってボタンを全て取得
+ボタンをforeachで回して、addEventListenerを使ってクリックしたエレメントのIDを取得
+2のIDをproductのidと合っているか評価する
+合っていたら表示する用のカートに追加する (表示用の配列と表示用に追加する為の配列を用意する)
+カートに追加したアイテムをdomで表示する
+
+4の追加するための配列は毎回、中身をクリアする
+*わからなくなったらconsole.log()等を使って返ってくる値を確認する。
+*2でthisを使うとボタンのNodeが返ってくるはずだからthis.nodeParentをつけてdivのNodeの情報をconsoleで出すように
+*3で2の時にとったNodeのidをproduct.idと等しいかifで評価する。 trueならカートへ
+*/
