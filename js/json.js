8e8ed1b8363ef44431911c375a4d3c69d17b3a7b
haruka <haruka.1017.ishikawa@gmail.com>
2023-02-08 21:55:50 -0800

    backup

---
+++ js/json.js
@@ -0,0 +1,100 @@
+
+[
+  {
+  "id": 0,
+  "name": "Cotton T-shirt",
+  "price": "CAD $ 90.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "./img/object_img/AA-1.jpg",
+  },
+  {
+  "id": 1,
+  "name": "Cotton Long-sleeved",
+  "price": "CAD $ 80.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/CC-1.jpg",
+  },
+  {
+  "id": 2,
+  "name": "Sports Hoodie",
+  "price": "CAD $ 100.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "./img/object_img/DD-2.jpg",
+  },
+  {
+  "id": 3,
+  "name": "Hoodie",
+  "price": "CAD $ 120.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/BB-1.jpg",
+  },
+  {
+  "id": 4,
+  "name": "Sports Hoodie",
+  "price": "CAD $ 100.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/DD-1.jpg",
+  },
+  {
+  "id": 5,
+  "name": "Cotton T-shirt",
+  "price": "CAD $ 90.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/AA-2.jpg",
+  },
+  {
+  "id": 6,
+  "name": "Sports Top",
+  "price": "CAD $ 97.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/EE-1.jpg",
+  },
+  {
+  "id": 7,
+  "name": "Sports Shorts",
+  "price": "CAD $ 50.00",
+  "instock": 10,
+  "description": "★★★★★",
+  "imgSrc": "img/object_img/EE-2.jpg",
+  },
+];
+
+[
+  {
+    "id": "pickup-0",
+    "name": "Colorful Geometric Patterns",
+    "imgSrc": "./img/object_img/A-1.jpg",
+  },
+  {
+    "id": "pickup-1",
+    "name": "Gingko Print",
+    "imgSrc": "./img/object_img/B-1.jpg",
+  },
+  {
+    "id": "pickup-2",
+    "name": "Fan Shape",
+    "imgSrc": "./img/object_img/E-1.jpg",
+  },
+  {
+    "id": "pickup-3",
+    "name": "Sun Flower",
+    "imgSrc": "./img/object_img/C-2.jpg",
+  },
+  {
+    "id": "pickup-4",
+    "name": "Ripple on Water",
+    "imgSrc": "./img/object_img/D-1.jpg",
+  },
+  {
+    "id": "pickup-5",
+    "name": "Colorful Geometric Patterns",
+    "imgSrc": "./img/object_img/A-2.jpg",
+  },
+];
