1baba82efb377caadf1d5599d1e0bca989c2ccc0
haruka <haruka.1017.ishikawa@gmail.com>
2023-02-09 22:39:01 -0800

    backup

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,19 +1,69 @@
-const btn = document.querySelectorAll(".addbtn")
+// CART
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
 
-function getBtnData () {
-  btn.forEach(() =>{
-    console.log(btn)
-  })
+//Open Cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+//Close Cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+//Cart Working JS
+if (document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded',ready)
+}else{
+  ready();
 }
 
-/*
-// 
-//ADD TO CART
-function addTOCart(id){
-  console.log(id);
+//Making Function
+function ready() {
+  //Remove Items From Cart
+  var removeCartButtons = document.getElementsByClassName('cart-remove')
+  console.log(removeCartButtons)
+  for (var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons[i]
+    button.addEventListener('click', removeCartItem)
+  }
 }
 
-*/
+  //Remove Items From Cart
+  function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+  }
+
+  //Update Total
+  function upDataTotal(){
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    var cartBoxes = cartContent.getElementsByClassName('cart-box')
+    for (var i = 0; i < cartBoxes.length; i++){
+      var cartBox = cartBoxes[i];
+      var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+      var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+      var price = parseFloat(priceElement.innerText.replase("$",""));
+      var quantity = quantityElement.value;
+      
+      document.getElementsByClassName('total-price')[0].innerText = "$" + total;
+    }
+  }
+
+
+
+
+
+
+// const btn = document.querySelectorAll(".addbtn")
+
+// function getBtnData () {
+//   btn.forEach(() =>{
+//     console.log(btn)
+//   })
+// }
+
 
 // //SERECT ELEMENTS
 // const productsEl = document.querySelector(".item");
